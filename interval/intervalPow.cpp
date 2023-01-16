1cc3a1f30b445a9f3feb80244d4c1b7ca04f01e0
Yann Orlarey <orlarey@gmail.com>
2023-01-16 18:23:48 +0100

    Add Apache-2.0 license notice

--- interval/intervalPow.cpp
+++ interval/intervalPow.cpp
@@ -1,3 +1,17 @@
+/* Copyright 2023 Yann ORLAREY
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include <algorithm>
 #include <cassert>
 #include <functional>
@@ -56,9 +70,8 @@ interval interval_algebra::Pow(const interval& x, const interval& y) const
 {
     if (x.lo() > 0) {
         return fPow(x, y);
-    } else {
-        return iPow(x, y);
     }
+    return iPow(x, y);
 }
 
 static double myfPow(double x, double y)
