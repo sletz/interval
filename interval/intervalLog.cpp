7a75d08a1e34727304b70651f6cd30805c3036f2
Agathe Herrou <herrou@MacBook-Pro.local>
2023-01-23 17:44:16 +0100

    Compute precision for the log function

--- interval/intervalLog.cpp
+++ interval/intervalLog.cpp
@@ -30,13 +30,19 @@ interval interval_algebra::Log(const interval& x) const
 {
     if (x.isEmpty()) return {};
 
+    int precision = x.lsb() - log2(x.hi());
+    int truncated_precision = std::max(precision, -24);
+
     interval i = intersection(interval(0, HUGE_VAL), x);
-    return {log(i.lo()), log(i.hi())};
+    return {log(i.lo()), log(i.hi()), precision};
 }
 
 void interval_algebra::testLog() const
 {
-    analyzeUnaryMethod(10, 1000, "log", interval(0, 10), log, &interval_algebra::Log);
+    analyzeUnaryMethod(10, 10000, "log", interval(0, 10, -6), log, &interval_algebra::Log);
+    analyzeUnaryMethod(10, 10000, "log", interval(0, 10, -12), log, &interval_algebra::Log);
+    analyzeUnaryMethod(10, 10000, "log", interval(0, 10, -20), log, &interval_algebra::Log);
+    analyzeUnaryMethod(10, 10000, "log", interval(0, 10, -24), log, &interval_algebra::Log);    
 
     // check("test algebra Log", Log(interval(1, 10)), interval(log(1), log(10)));
     // check("test algebra Log", Log(interval(0, 10)), interval(log(0), log(10)));
