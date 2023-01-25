88d2ed85f8904025676c22175334eac0141f0f1d
Yann Orlarey <orlarey@gmail.com>
2023-01-25 10:05:18 +0100

    Missing climits include

--- interval/check.cpp
+++ interval/check.cpp
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <climits>
 #include <iostream>
 #include <random>
 #include <sstream>
